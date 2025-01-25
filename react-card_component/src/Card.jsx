d0ddc72f3eaeddc31395d21598b1fff7bb4c739d
Rajlodh-001 <Rajlodh911@gmail.com>
2025-01-25 21:27:44 +0530

    Added card component

---
+++ react-card_component/src/Card.jsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+function Card (){
+
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="card Image" />
+            <h2 className="card-title">Card Title</h2>
+            <p className="card-text"> <span style={{fontWeight: "bold"}}>Card Text </span>- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Axplicabo architecto officiis a eveniet Molestiae reprehenderit assumenda rem odio.</p>
+        </div>
+    );
+
+}
+
+
+
+export default Card
