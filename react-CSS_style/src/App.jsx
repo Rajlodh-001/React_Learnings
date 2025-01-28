fdcffe481dd92c5b629618ad3a2aaf44f664bf36
Rajlodh-001 <Rajlodh911@gmail.com>
2025-01-28 12:12:01 +0530

    Css -styls

---
+++ react-CSS_style/src/App.jsx
@@ -0,0 +1,28 @@
+
+
+// How to style react componebts with css
+// --------------------------------------
+// (not including extrnal frameworks or preprocessors)
+ 
+// 1. EXTERNAL CSS 
+// 2. MODULES CSS
+// 3. INLINE CSS
+
+
+
+
+import ButtonM from './Button/ButtonM.jsx'
+import ButtonE from './ButtonE.jsx'
+import ButtonI from './ButtonI.jsx'
+function App() {
+
+  return(
+    <>
+    <ButtonM/>
+    <ButtonE/>
+    <ButtonI/>
+    </>
+  )
+}
+
+export default App
---
+++ react-CSS_style/src/Button/ButtonM.jsx
@@ -0,0 +1,15 @@
+
+
+import styles from './ButtonM.module.css'
+
+function ButtonM (){
+
+return(
+ <button className={styles.button}>Click me M</button> 
+);
+
+
+
+}
+
+export default ButtonM
